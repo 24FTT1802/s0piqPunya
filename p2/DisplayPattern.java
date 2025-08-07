b7c1f74fce174650ae08221765b24229aad8a10d
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-07 14:57:30 +0800

    Update DisplayPattern.java

--- p2/DisplayPattern.java
+++ p2/DisplayPattern.java
@@ -1,10 +1,12 @@
 public class DisplayPattern {
     public static void main(String[] args){
         
-        System.out.println("(\\__/) I am an");
-        System.out.println("( @w@)       OOO  W     W L");
-        System.out.println("/) W )      O   O  W W W  L");
-        System.out.println(" ^  ^        OOO    W W   L L L");
+    System.out.println("""
+                (\\__/) I am an
+                ( @w@)       OOO  W     W L
+                /) W )      O   O  W W W  L
+                 ^  ^        OOO    W W   L L L
+                """);
 
     }    
 }
