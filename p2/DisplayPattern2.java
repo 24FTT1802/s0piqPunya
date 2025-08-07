ff41e740f6e9c4ca1f94f6b1804ab86dd73fdc83
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-07 14:30:27 +0800

    Create DisplayPattern2.java

---
+++ p2/DisplayPattern2.java
@@ -0,0 +1,15 @@
+public class DisplayPattern2 {
+    public static void main(String[] args) {
+        System.out.println("""
+                   .=""=.
+                  /      \\
+                 | O    O |
+                 \\   /\\   /
+                 /'-=\\/=-'\\
+                / /      \\ \\      CHIP CHIP!
+                \\/\\      /\\/
+                   '.  .'
+                   /|\\/|\\
+                """);
+    }
+}
