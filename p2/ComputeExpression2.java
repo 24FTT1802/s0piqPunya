902e81bd3da4c4155317c8e7930b790d6156fca9
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-07 14:29:48 +0800

    Create ComputeExpression2.java

---
+++ p2/ComputeExpression2.java
@@ -0,0 +1,10 @@
+public class ComputeExpression2 {
+    public static void main(String[] args) {
+        
+        double num = (7.5 * 2.5 - 1.5 * 3) / (4 - 2.5);
+        
+        System.out.println("The result of 7.5 x 2.5 - 1.5 x 3");
+        System.out.println("              -------------------   is " + num);
+        System.out.println("                  4 - 2.5");
+    }
+}
