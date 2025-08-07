ba4663713a63503a9ccdeec988049d8be7eb64c3
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-07 15:09:24 +0800

    Update ComputeExpression2.java

--- p2/ComputeExpression2.java
+++ p2/ComputeExpression2.java
@@ -3,8 +3,10 @@ public class ComputeExpression2 {
         
         double num = (7.5 * 2.5 - 1.5 * 3) / (4 - 2.5);
         
-        System.out.println("The result of 7.5 x 2.5 - 1.5 x 3");
-        System.out.println("              -------------------   is " + num);
-        System.out.println("                  4 - 2.5");
+        System.out.printf("""
+                The result of 7.5 x 2.5 - 1.5 x 3
+                              -------------------   is %.1f
+                                  4 - 2.5""", num);
+        
     }
 }
