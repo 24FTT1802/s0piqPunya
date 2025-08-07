a30a1a7cd22c3d99c8f57aa0940dc637b8726a10
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-07 14:33:20 +0800

    Create ConvertBytesIntoBits.java

---
+++ p3/ConvertBytesIntoBits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ConvertBytesIntoBits {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int bytes;
+        int bits;
+
+        System.out.print("Enter the number of byte(s) to be converted: ");
+        bytes = input.nextInt();
+
+        bits = bytes * 8;
+
+        System.out.printf("%d byte(s) is %d bits", bytes, bits);
+
+        input.close();
+    }
+}
