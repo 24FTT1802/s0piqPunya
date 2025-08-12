c1d317c966e9576588921d6b6dc128ee93983ab3
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-13 00:19:53 +0800

    Update QueueReceipt.java

--- p5/QueueReceipt.java
+++ p5/QueueReceipt.java
@@ -1,8 +1,10 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+// import java.text.SimpleDateFormat;
 import java.util.Scanner;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+// import java.util.Date;
 
 public class QueueReceipt {
     public static void main(String[] args) throws IOException {
@@ -10,17 +12,19 @@ public class QueueReceipt {
         Scanner input = new Scanner(System.in);
         FileWriter writer = new FileWriter("C:\\Users\\ACER-1745\\Desktop\\Skewl\\java\\itp-class-activity\\Practical 5/QReceipt.txt");
 
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
-        String date = sdf.format(new Date());
+        // SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
+        // String date = sdf.format(new Date());
         
         System.out.print("Enter name: ");
         String name = input.nextLine();
-        System.out.println("Receipt generated. Thank you");
-
+        
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss"));
         int num = (int)(Math.random() * 100);
 
         writer.write(date + "\n\n Hi, " + name + "\n\n Your queue No is : " + num);
 
-            input.close(); writer.close();
+        System.out.println("Receipt generated. Thank you");
+
+        input.close(); writer.close();
     }
 }
