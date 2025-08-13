e455f5dcebc6a3afea474bdbae63aeca3721345b
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-13 13:41:06 +0800

    Update RockPaperScissor.java

--- p6/RockPaperScissor.java
+++ p6/RockPaperScissor.java
@@ -10,48 +10,46 @@ public class RockPaperScissor {
 
         Scanner input = new Scanner(System.in);
 
+        int choice;
         int ans = (int)(Math.random() * Integer.MAX_VALUE) % 3;
 
-        System.out.println("Enter rock (0), paper (1), scissor (2)");
-        int choice = input.nextInt();
+        do{
+            System.out.println("Enter rock (0), paper (1), scissor (2)");
+            choice = input.nextInt();
+        } while (choice != 0 && choice != 1 && choice != 2);
 
-        if (choice == 0 || choice == 1 || choice == 2){
-            if (ans == 0){
-                if (choice == 0){
-                    System.out.println("The computer is rock. You are rock. It is a draw.");
-                }
-                else if (choice == 1){
-                    System.out.println("The computer is rock. You are paper. You Win.");
-                }
-                else if (choice == 2){
-                    System.out.println("The computer is rock. You are scissor. You Lose.");
-                }
+        if (ans == 0){
+            if (choice == 0){
+                System.out.println("The computer is rock. You are rock. It is a draw.");
             }
-            if (ans == 1){
-                if (choice == 0){
-                    System.out.println("The computer is paper. You are rock. You Lose.");
-                }
-                else if (choice == 1){
-                    System.out.println("The computer is paper. You are paper. It is a draw.");
-                }
-                else if (choice == 2){
-                    System.out.println("The computer is paper. You are scissor. You Win.");
-                }
+            else if (choice == 1){
+                System.out.println("The computer is rock. You are paper. You Win.");
             }
-            if (ans == 2){
-                if (choice == 0){
-                    System.out.println("The computer is scissor. You are rock. You Win.");
-                }
-                else if (choice == 1){
-                    System.out.println("The computer is scissor. You are paper. You Lose.");
-                }
-                else if (choice == 2){
-                    System.out.println("The computer is scissor. You are scissor. You It is a draw.");
-                }
+            else if (choice == 2){
+                System.out.println("The computer is rock. You are scissor. You Lose.");
             }
         }
-        else {
-            System.out.println("What the helly");
+        if (ans == 1){
+            if (choice == 0){
+                System.out.println("The computer is paper. You are rock. You Lose.");
+            }
+            else if (choice == 1){
+                System.out.println("The computer is paper. You are paper. It is a draw.");
+            }
+            else if (choice == 2){
+                System.out.println("The computer is paper. You are scissor. You Win.");
+            }
+        }
+        if (ans == 2){
+            if (choice == 0){
+                System.out.println("The computer is scissor. You are rock. You Win.");
+            }
+            else if (choice == 1){
+                System.out.println("The computer is scissor. You are paper. You Lose.");
+            }
+            else if (choice == 2){
+                System.out.println("The computer is scissor. You are scissor. You It is a draw.");
+            }
         }
 
         input.close();
