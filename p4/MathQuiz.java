050f246d9976bd5582b248fd9582fa5fc0c96691
Syafiq <24ftt1802@student.pb.edu.bn>
2025-08-28 20:34:29 +0800

    Create MathQuiz.java

---
+++ p4/MathQuiz.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MathQuiz {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int maxNum = 100;
+
+        int num1 = (int)(Math.random() * (maxNum+1));
+        int num2 = (int)(Math.random() * (maxNum+1));
+
+        System.out.printf("What is %d + %d ?\n", num1, num2);
+        int ansSum = input.nextInt();
+        System.out.printf("%d + %d = %d ? %b\n", num1, num2, ansSum, (num1+num2));
+
+        
+        System.out.printf("\nWhat is %d %% %d ?\n", num1, num2);
+        int ansModul = input.nextInt();
+        System.out.printf("%d %% %d = %d ? %b\n", num1, num2, ansModul, (num1%num2));
+
+        input.close();
+    }
+}
