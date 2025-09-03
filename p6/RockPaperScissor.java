638a6eb40a78849bc50d0bab0ebbca96bea537b1
Syafiq <24ftt1802@student.pb.edu.bn>
2025-09-03 21:08:34 +0800

    Update RockPaperScissor.java

--- p6/RockPaperScissor.java
+++ p6/RockPaperScissor.java
@@ -12,22 +12,22 @@ public class RockPaperScissor {
         if (ans == 0){
             switch (choice) {
                 case 0: System.out.println("The computer is rock. You are rock. It is a draw."); break;
-                case 1: System.out.println("The computer is rock. You are Scissor. You Lose."); break;
-                case 2: System.out.println("The computer is rock. You are paper. You Win."); break;
+                case 1: System.out.println("The computer is rock. You are paper. You Win."); break;
+                case 2: System.out.println("The computer is rock. You are scissor. You Lose."); break;
             }
         }
         else if (ans == 1){
             switch (choice){
                 case 0: System.out.println("The computer is scissor. You are rock. You Win."); break;
-                case 1: System.out.println("The computer is scissor. You are scissor. It is a draw."); break;
-                case 2: System.out.println("The computer is scissor. You are paper. You Lose."); break;
+                case 1: System.out.println("The computer is scissor. You are paper. You Lose."); break;
+                case 2: System.out.println("The computer is scissor. You are scissor. It is a draw."); break;
             }
         }
         else if (ans == 2){
             switch (choice){
                 case 0: System.out.println("The computer is paper. You are rock. You Lose."); break;
-                case 1: System.out.println("The computer is paper. You are scissor. You Win."); break;
-                case 2: System.out.println("The computer is paper. You are paper. It is a draw."); break;
+                case 1: System.out.println("The computer is paper. You are paper. It is a draw."); break;
+                case 2: System.out.println("The computer is paper. You are scissor. You Win."); break;
             }
         }
 
