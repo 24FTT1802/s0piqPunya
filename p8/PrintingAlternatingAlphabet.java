50615c76c2db7b130a3be36230381404270f1ea7
Syafiq <24ftt1802@student.pb.edu.bn>
2025-09-05 03:18:45 +0800

    Create PrintingAlternatingAlphabet.java

---
+++ p8/PrintingAlternatingAlphabet.java
@@ -0,0 +1,18 @@
+package P8;
+
+import java.util.Scanner;
+
+public class PrintingAlternatingAlphabet {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        for (int i = 0; i < alphabet.length; i++){
+            System.out.print(alphabet[i] + " ");
+        }
+
+        input.close();
+    }
+}
